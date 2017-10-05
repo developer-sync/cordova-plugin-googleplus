46823b40487fe6567f83abc9e4b899dafce1635b
harry_pennont <hpennont@sync.tv>
2017-10-05 14:42:43 +0200

    add import for GoogleSignInStatusCodes

--- src/android/GooglePlus.java
+++ src/android/GooglePlus.java
@@ -11,6 +11,7 @@ import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInStatusCodes;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
